f1eec652b2bbb81a4f4b497f7bf22cb26df89387
Cian Payne <cianjpayne@gmail.com>
2020-02-17 17:01:46 +0000

    Created main GUI form

---
+++ src/com/tuturu/mum/client/MainForm.java
@@ -0,0 +1,19 @@
+package com.tuturu.mum.client;
+
+import javax.swing.*;
+
+public class MainForm
+{
+    private JTextField hostnameField;
+    private JTextField portField;
+    private JTextField usernameField;
+    private JButton connectButton;
+    private JTextArea messageArea;
+    private JButton sendButton;
+    private JTextField messageTextField;
+
+    private void createUIComponents()
+    {
+        // TODO: place custom component creation code here
+    }
+}
