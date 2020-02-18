bfc98ded3410a6dcd26d87b4d0c89ba779ba488c
Cian Payne <cianjpayne@gmail.com>
2020-02-18 11:33:34 +0000

    Implemented Client functionality in MainForm Changed a couple of method signatures in Client

--- src/com/tuturu/mum/client/Client.java
+++ src/com/tuturu/mum/client/Client.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 public class Client
 {
     private String username;
-    private final Thread messageListener;
-    private final DataInputStream in;
-    private final DataOutputStream out;
+    private Thread messageListener;
+    private DataInputStream in;
+    private DataOutputStream out;
 
-    public Client(String hostname, int port, String username) throws IOException
+    public void connect(String hostname, int port, String username) throws IOException
     {
         Socket server = new Socket(hostname, port);
         this.in = new DataInputStream(server.getInputStream());
@@ -36,23 +36,22 @@ public class Client
         }
     }
 
-    private void sendMessage(DataOutputStream out, String mType,
-                             String content) throws IOException
+    public void sendMessage(String mType, String content) throws IOException
     {
         String message = mType + "," + this.username + "," + content;
-        out.writeUTF(message);
-        out.flush();
+        this.out.writeUTF(message);
+        this.out.flush();
     }
 
     private void setUsername(String username) throws IOException
     {
-        this.sendMessage(this.out, "CONN_REQ", "");
+        this.sendMessage("CONN_REQ", "");
         String[] res = this.in.readUTF().split(",", 2);
         String res_type = res[0];
         String res_message = res[2];
 
         if (res_type.equals("CONN_REFUSED"))
-            throw new IOException("Error: connection refused - "+res_message);
+            throw new IOException("Error: connection refused - " + res_message);
         else if (res_type.equals("CONN_ACCEPT"))
             this.username = username;
         /*
--- src/com/tuturu/mum/client/MainForm.java
+++ src/com/tuturu/mum/client/MainForm.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 public class MainForm extends JFrame
 {
@@ -21,6 +22,8 @@ public class MainForm extends JFrame
     private JPanel formPanel;
     private JScrollPane messageScrollPane;
     private JPanel messagePanel;
+
+    private final Client client = new Client();
 
     public static void main(String[] args) {
         new MainForm().createUIComponents();
@@ -53,6 +56,16 @@ public class MainForm extends JFrame
             {
                 messageArea.append(messageTextField.getText() + '\n');
                 messageArea.setRows(messageArea.getRows() + 1);
+                try
+                {
+                    client.connect(hostnameField.getText(),
+                                   Integer.parseInt(portField.getText()),
+                                   usernameField.getText());
+                }
+                catch (IOException e)
+                {
+                    messageArea.append(e.getMessage());
+                }
             }
         });
 
@@ -66,6 +79,15 @@ public class MainForm extends JFrame
             {
                 messageArea.append(messageTextField.getText() + '\n');
                 messageArea.setRows(messageArea.getRows() + 1);
+                messageTextField.setText("");
+                try
+                {
+                    client.sendMessage("USR_MSG", messageTextField.getText());
+                }
+                catch (IOException e)
+                {
+                    messageArea.append(e.getMessage());
+                }
             }
         });
 
@@ -80,9 +102,17 @@ public class MainForm extends JFrame
             public void keyReleased(KeyEvent keyEvent)
             {
                 if(keyEvent.getKeyCode() == Event.ENTER && !messageTextField.getText().isEmpty()) {
-                    messageArea.append(messageTextField.getText() + '\n');
+                    messageArea.append("You: " + messageTextField.getText() + '\n');
                     messageArea.setRows(messageArea.getRows() + 1);
                     messageTextField.setText("");
+                    try
+                    {
+                        client.sendMessage("USR_MSG", messageTextField.getText());
+                    }
+                    catch (IOException e)
+                    {
+                        messageArea.append(e.getMessage());
+                    }
                 }
             }
         });
@@ -90,5 +120,10 @@ public class MainForm extends JFrame
         this.setContentPane(this.mainPanel);
         this.pack();
         this.setVisible(true);
+    }
+
+    public void addMessage(String message)
+    {
+        this.messageArea.append(message);
     }
 }
