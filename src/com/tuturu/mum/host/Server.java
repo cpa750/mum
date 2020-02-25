4f5ae1cf2f37d0f5e67e2eee10cd07297d44689d
Cian Payne <cianjpayne@gmail.com>
2020-02-25 17:32:41 +0000

    Refactored server code

--- src/com/tuturu/mum/host/Server.java
+++ src/com/tuturu/mum/host/Server.java
@@ -1,7 +1,5 @@
 package com.tuturu.mum.host;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,39 +8,16 @@ import java.util.concurrent.ConcurrentHashMap;
 // TODO: rewrite this absolute mess
 public class Server
 {
-    private ConcurrentHashMap<String, Socket> clients = new ConcurrentHashMap<>();
-
     public static void main(String[] args)
     {
+        // TODO: multithreading
+        ConcurrentHashMap<String, Socket> clients = new ConcurrentHashMap<>();
         int port = Integer.parseInt(args[0]);
         try ( ServerSocket serverSocket = new ServerSocket(port) )
         {
-            do
-            {
-                Socket clientSocket = serverSocket.accept();
-                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-
-                // Message format: type,username,content
-                String rec = in.readUTF();
-                System.out.println(rec);
-                String[] split = rec.split(",", 3);
-                String messageType = split[0];
-                String username = split[1];
-
-                if (messageType.equals("CONN_REQ") && !clients.containsKey(username))
-                {
-                    out.writeUTF("CONN_ACCEPT,Server,pong");
-                    out.flush();
-                    ClientThread clientThread =
-                            new ClientThread(clientSocket, in, out, username);
-                    new Thread(clientThread).start();
-                }
-                else
-                {
-                    out.writeUTF("CONN_REFUSED,Server,Non-unique username");
-                    out.flush();
-                }
+            do {
+                Socket client = serverSocket.accept();
+                new ClientThread(client, clients).run();
             } while (clients.size() > 0);
         }
         catch (IOException e)
