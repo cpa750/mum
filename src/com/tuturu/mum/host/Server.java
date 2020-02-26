1bb96b68433bb08314966693c15adc9ecc744e78
Cian Payne <cianjpayne@gmail.com>
2020-02-26 17:07:45 +0000

    Added server side threadpooling

--- src/com/tuturu/mum/host/Server.java
+++ src/com/tuturu/mum/host/Server.java
@@ -4,19 +4,22 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server
 {
     public static void main(String[] args)
     {
-        // TODO: multithreading
         ConcurrentHashMap<String, Socket> clients = new ConcurrentHashMap<>();
         int port = Integer.parseInt(args[0]);
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
         try ( ServerSocket serverSocket = new ServerSocket(port) )
         {
             do {
                 Socket client = serverSocket.accept();
-                new ClientThread(client, clients).run();
+                executor.execute( new ClientThread(client, clients) );
             } while (clients.size() > 0);
         }
         catch (IOException e)
